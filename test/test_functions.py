18ac35211b7dc3fda86da2ff6a56b9d6b9501ae3
punkyfer <punkyfer666@gmail.com>
2020-02-24 20:41:28 +0100

    Fixed broken test

--- test/test_functions.py
+++ test/test_functions.py
@@ -13,18 +13,18 @@ class TestTrivialCase(unittest.TestCase):
 		# Test that it returns (True, 1) when there are no negated variables in the clauses
 		pos_clauses = [[1,2,3],[2,1,4],[3,4,2],[4,3,1]]
 		result = fn.trivial_case(pos_clauses)
-		self.assertEqual(result, (True, 1))
+		self.assertEqual(result, 1)
 
 	def test_all_clauses_negated(self):
 		# Test that it returns (False, 0) when all variables in the clauses are negated
 		neg_clauses = [[-1,-2,-3],[-2,-1,-4],[-3,-4,-2],[-4,-3,-1]]
 		result = fn.trivial_case(neg_clauses)
-		self.assertEqual(result, (True, 0))
+		self.assertEqual(result, 0)
 
 	def test_mixed_clauses(self):
 		clauses = [[1,2,3],[2,1,4],[3,4,2],[4,3,1],[-1,-2,-3],[-2,-1,-4],[-3,-4,-2],[-4,-3,-1]]
 		result = fn.trivial_case(clauses)
-		self.assertEqual(result, (False, -1))
+		self.assertEqual(result, -1)
 
 
 class TestRemoveUnitVars(unittest.TestCase):
